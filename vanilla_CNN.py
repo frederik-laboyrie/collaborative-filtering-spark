c984882a778b0023084772140144c123e11131c7
Frederik <frederiklaboyrie1@gmail.com>
2017-09-03 16:10:58 +0200

    moving some functions to preprocessing

--- vanilla_CNN.py
+++ vanilla_CNN.py
@@ -1,21 +1,8 @@
-import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
-
-def get_input_shape(data):
-    num_samples = data.shape[0]
-    channels = 3
-    img_rows = data.shape[2]
-    img_cols = data.shape[3]
-    return (num_samples,img_rows, img_cols, channels)
-
-def reshape(data):
-    return np.reshape(data, get_input_shape(data))
-
-def subsample(data, labels, nb_samples):
-    return data[:nb_samples], labels[:nb_samples]
+from data_preprocessing import get_input_shape
 
 def vanilla_CNN(filters, kernel_size, data):
     model = Sequential()
@@ -34,4 +21,3 @@ def vanilla_CNN(filters, kernel_size, data):
     model.add(Dense(2,activation ='linear'))
     model.compile(loss = 'mean_absolute_error', optimizer='adam')
     return model
-
