567c10b6a1e28a0fa153eb9ec14c204424fa6dda
Frederik <frederiklaboyrie1@gmail.com>
2017-09-03 18:21:00 +0200

    reshape update

--- vanilla_training.py
+++ vanilla_training.py
@@ -3,9 +3,8 @@ from vanilla_CNN import *
 import sys
 
 def main():
-	x, y = load_standardized_singleres()
+	xs, ys = load_standardized_singleres()
 	model = vanilla_CNN(int(sys.argv[1]),int(sys.argv[2]),xs)	
-	xs = reshape(xs)
 	model.fit(xs,ys)
 
 if __name__ == '__main__':
