5d001cf661f650a2bd3ea2c2a0bbe50b8487afc0
Frederik Laboyrie <frederiklaboyrie1@gmail.com>
2017-11-28 21:52:10 +0100

    downscale

---
+++ modelling/greyscale_downscale.py
@@ -0,0 +1,11 @@
+""" preprocess beforehand to store on cloud to manage memory better during training """
+
+from scipy.misc import imresize
+
+images = np.load('AllBW.npy')
+
+images64 = np.array([imresize(image,(64,64)) for image in images])
+images32 = np.array([imresize(image,(32,32)) for image in images])
+
+np.save('AllImagesBW64.npy', images64)
+np.save('AllImagesBW32.npy', images32)
